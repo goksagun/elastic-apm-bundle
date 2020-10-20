fc27fc1ee14b19a45057466a34443a9eeab362d7
Christoph Quadt <quadt@united-domains.de>
2020-10-20 13:26:04 +0200

    Added compiler pass for apm agent.

--- ElasticApmBundle.php
+++ ElasticApmBundle.php
@@ -2,8 +2,19 @@
 
 namespace Goksagun\ElasticApmBundle;
 
+use Goksagun\ElasticApmBundle\DependencyInjection\Compiler\ApmAgentCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ElasticApmBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ApmAgentCompilerPass());
+    }
 }
